5e1a58f50c3ad861404a5e63fd2aba05f2a26b73
Cayprol <Cayprol@users.noreply.github.com>
2017-12-15 13:52:03 +0800

    Delete change service.cmd

--- change service.cmd
+++
@@ -1,9 +0,0 @@
-sc stop "AdobeARMservice" 
-sc stop "AGSService"
-
-sc config "AdobeARMservice" start=disabled
-sc config "AGSService" start=disabled
-
-
-REG ADD "HKLM\SOFTWARE\WOW6432Node\Adobe\Adobe ARM\Legacy\Acrobat\{AC76BA86-1033-FFFF-7760-0C0F074E4100}" /v Mode /t REG_DWORD /d 0 /f
-REG ADD "HKLM\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown" /v bUpdater /t REG_DWORD /d 0 /f
